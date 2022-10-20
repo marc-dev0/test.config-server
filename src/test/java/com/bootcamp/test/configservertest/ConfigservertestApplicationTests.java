5f46790242d6900c85d3c0604bf1a3f475a2819f
marc-dev2 <115658108+marc-dev2@users.noreply.github.com>
2022-10-20 14:48:48 -0500

    initial commit

---
+++ src/test/java/com/bootcamp/test/configservertest/ConfigservertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.test.configservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigservertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
